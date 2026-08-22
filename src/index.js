85b375ac93824c4290c4958ca72e0bd3ad4f9ff7
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-08-22 15:58:29 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import config from "./config/index.js";
 import { logger } from "./core/logger.js";
-import {
-  connectWhatsApp,
-  disconnectWhatsApp
-} from "./core/connection.js";
+import { connectWhatsApp } from "./core/connection.js";
 import { loadCommands } from "./core/command-loader.js";
+import { initStorage } from "./storage/index.js";
 
 async function bootstrap() {
   logger.info("Starting NovaBot...");
@@ -12,31 +10,22 @@ async function bootstrap() {
   logger.info(`Bot Name: ${config.bot.name}`);
   logger.info(`Prefix: ${config.command.prefix}`);
 
+  await initStorage();
   await loadCommands();
   await connectWhatsApp();
 }
 
-async function shutdown(signal) {
+function shutdown(signal) {
   logger.info(`${signal} received.`);
   logger.info("Shutting down NovaBot...");
-
-  disconnectWhatsApp();
 
   process.exit(0);
 }
 
-process.on("SIGINT", () => {
-  shutdown("SIGINT");
-});
-
-process.on("SIGTERM", () => {
-  shutdown("SIGTERM");
-});
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
 
 bootstrap().catch((error) => {
-  logger.error(
-    `Fatal startup error: ${error.message}`
-  );
-
+  logger.error(`Fatal startup error: ${error.message}`);
   process.exit(1);
 });
