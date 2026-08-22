77c18f6b0a4595e5e60d01629045518ce8626c4d
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-08-22 20:29:44 +0700

    Refactor sticker command structure and error handling

--- src/plugins/sticker/index.js
+++ src/plugins/sticker/index.js
@@ -1,29 +1,40 @@
 import { createSticker } from "../../media/sticker.js";
 
-const stickerCommand = {
-  name: "sticker",
-  aliases: ["s"],
-  category: "media",
-  description: "Mengubah gambar atau video menjadi sticker.",
-
-  async execute({ message, reply, socket, jid }) {
-    try {
-      const sticker = await createSticker(message);
-
-      await socket.sendMessage(jid, {
-        sticker
-      });
-    } catch (error) {
-      await reply(
-        `Sticker gagal dibuat: ${error.message}`
-      );
-    }
-  }
-};
-
 export default {
   name: "sticker",
+
   commands: [
-    stickerCommand
+    {
+      name: "sticker",
+      aliases: ["s"],
+      category: "media",
+      description: "Membuat sticker dari gambar atau video",
+
+      async execute({
+        message,
+        socket,
+        jid,
+        reply
+      }) {
+        try {
+          const sticker = await createSticker(
+            message
+          );
+
+          await socket.sendMessage(
+            jid,
+            {
+              sticker
+            }
+          );
+        } catch (error) {
+          await reply(
+            `Sticker gagal dibuat: ${error.message}`
+          );
+
+          throw error;
+        }
+      }
+    }
   ]
 };
